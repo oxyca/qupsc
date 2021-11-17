9ae576eb8caea2b720289053f4c5d5a0e4d154e0
Andrey Borovsky <andrei@borovsky@gmail.com>
2021-11-17 14:17:46 +0300

    Update CMakeLists.txt and qupsc_ru_RU.ts

--- src/qupsc_ru_RU.ts
+++ src/qupsc_ru_RU.ts
@@ -5,13 +5,11 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="qupsc_autogen/include/ui_mainwindow.h" line="93"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="81"/>
-        <location filename="qupsc_autogen/include/ui_mainwindow.h" line="94"/>
         <source>toolBar</source>
         <translation type="unfinished"></translation>
     </message>
